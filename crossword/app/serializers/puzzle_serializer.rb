db11a972d9b4e195bdb2ff7c75b4799fc3219542
shannon-nabors <shannon.nabors@gmail.com>
2019-04-05 10:21:02 -0400

    edited puzzle serializer to give across and down clues

--- crossword/app/serializers/puzzle_serializer.rb
+++ crossword/app/serializers/puzzle_serializer.rb
@@ -1,6 +1,25 @@
 class PuzzleSerializer < ActiveModel::Serializer
-  attributes :id, :title
+  attributes :id, :title, :correct_letters, :across_clues, :down_clues
   belongs_to :constructor
   has_many :cells
-  has_many :clues
+  # has_many :clues
+
+  def correct_letters
+    letters = {}
+    object.cells.each do |c|
+      if c.letter
+        letters[c.id] = c.letter
+      end
+    end
+    letters
+  end
+
+  def across_clues
+    return object.clues.select{ |c| c.direction == "across"}
+  end
+
+  def down_clues
+    return object.clues.select{ |c| c.direction == "down"}
+  end
+
 end
