77c4331bd86a16654ab74945edad70636d3616a5
shannon-nabors <shannon.nabors@gmail.com>
2019-04-09 17:24:18 -0400

    got two clue cells to be created for each cell

--- crossword/app/controllers/puzzles_controller.rb
+++ crossword/app/controllers/puzzles_controller.rb
@@ -53,39 +53,53 @@ class PuzzlesController < ApplicationController
 
         if cell.column == 1 || left.shaded == true
           cl = Clue.create(number: num, direction: "across", puzzle_id: params[:id])
+          ClueCell.create(clue: cl, cell: cell)
         end
 
         if cell.row == 1 || top.shaded == true
           cl = Clue.create(number: num, direction: "down", puzzle_id: params[:id])
+          ClueCell.create(clue: cl, cell: cell)
         end
 
         Cell.find(cell.id).update(number: num)
         num += 1
       end
 
-      index = cell.id - find_index
+      if cell.shaded == false
+        index = cell.id - find_index
 
-      while index >= 0
-        if cells[index].shaded || cells[index].column == 1
-          cells[index].shaded ? left_word_start = cells[index + 1] : left_word_start = cells[index]
-          index = cell.id - find_index
-          break
+        while index >= 0
+          if cells[index].shaded || cells[index].column == 1
+            cells[index].shaded ? left_word_start = cells[index + 1] : left_word_start = cells[index]
+          end
+          break if cells[index].shaded || cells[index].column == 1
+          index = index - 1
         end
-        index = index - 1
-      end
 
-      while index >= 0
-        if cells[index].shaded || cells[index].row == 1
-          cells[index].shaded ? top_word_start = cells[index + rows] : top_word_start = cells[index]
-          break
+        index = cell.id - find_index
+
+        while index >= 0
+          if cells[index].shaded || cells[index].row == 1
+            cells[index].shaded ? top_word_start = cells[index + rows] : top_word_start = cells[index]
+          end
+          break if cells[index].shaded || cells[index].row == 1
+          index = index - rows
         end
+
+        across_clue = left_word_start.clues.find{ |c| c.direction == "across"}
+        down_clue = top_word_start.clues.find{ |c| c.direction == "down"}
+
+
+        if !cell.clues.include?(across_clue)
+          ClueCell.create(clue: across_clue, cell: cell)
+        end
+
+        if !cell.clues.include?(down_clue)
+          ClueCell.create(clue: down_clue, cell: cell)
+        end
+
+        
       end
-
-      across_clue = left_word_start.clues.find{ |c| c.direction == "across"}
-      down_clue = top_word_start.clues.find{ |c| c.direction == "down"}
-
-      ClueCell.create(clue: across_clue, cell: cell)
-      ClueCell.create(clue: down_clue, cell: cell)
 
     end
     render json: Puzzle.find(params[:id])
