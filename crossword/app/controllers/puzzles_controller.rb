b4c71788dd9dc2ab50d7f63d4d405c30a7d89246
shannon-nabors <shannon.nabors@gmail.com>
2019-04-09 16:53:44 -0400

    trying to create two clue cells for each cell

--- crossword/app/controllers/puzzles_controller.rb
+++ crossword/app/controllers/puzzles_controller.rb
@@ -41,6 +41,8 @@ class PuzzlesController < ApplicationController
     end
 
     cells = puzzle.cells.sort_by{ |cell| cell.id }
+    find_index = cells[0].id
+    rows = cells.last.row
 
     cells.each do |cell|
       left = cells.find{ |c| c.row == cell.row && c.column == cell.column - 1}
@@ -48,19 +50,43 @@ class PuzzlesController < ApplicationController
       top = cells.find{ |c| c.column == cell.column  && c.row == cell.row - 1}
 
       if (cell.shaded == false) && ((cell.row == 1 || cell.column == 1) || left.shaded == true || top.shaded == true)
+
         if cell.column == 1 || left.shaded == true
           cl = Clue.create(number: num, direction: "across", puzzle_id: params[:id])
-          ClueCell.create(clue: cl, cell: cell)
         end
 
         if cell.row == 1 || top.shaded == true
           cl = Clue.create(number: num, direction: "down", puzzle_id: params[:id])
-          ClueCell.create(clue: cl, cell: cell)
         end
 
         Cell.find(cell.id).update(number: num)
         num += 1
       end
+
+      index = cell.id - find_index
+
+      while index >= 0
+        if cells[index].shaded || cells[index].column == 1
+          cells[index].shaded ? left_word_start = cells[index + 1] : left_word_start = cells[index]
+          index = cell.id - find_index
+          break
+        end
+        index = index - 1
+      end
+
+      while index >= 0
+        if cells[index].shaded || cells[index].row == 1
+          cells[index].shaded ? top_word_start = cells[index + rows] : top_word_start = cells[index]
+          break
+        end
+      end
+
+      across_clue = left_word_start.clues.find{ |c| c.direction == "across"}
+      down_clue = top_word_start.clues.find{ |c| c.direction == "down"}
+
+      ClueCell.create(clue: across_clue, cell: cell)
+      ClueCell.create(clue: down_clue, cell: cell)
+
     end
     render json: Puzzle.find(params[:id])
   end
--- crossword/app/serializers/cell_serializer.rb
+++ crossword/app/serializers/cell_serializer.rb
@@ -1,8 +1,6 @@
 require 'byebug'
 class CellSerializer < ActiveModel::Serializer
-  attributes :id, :shaded, :number, :letter, :row, :column
+  attributes :id, :shaded, :number, :letter, :row, :column, :clues
   belongs_to :puzzle
-
-
-
+  has_many :clues, through: :clue_cells
 end
