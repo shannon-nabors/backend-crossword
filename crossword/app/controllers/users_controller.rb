b65bad0c4667bf585e18a8614d2da5b43f137df4
shannon-nabors <shannon.nabors@gmail.com>
2019-04-10 21:55:11 -0400

    fixed user_puzzles bug

--- crossword/app/controllers/users_controller.rb
+++ crossword/app/controllers/users_controller.rb
@@ -2,9 +2,7 @@ class UsersController < ApplicationController
 
   def login
     @user = User.find_by(username: params[:username])
-    render json: {
-      user: @user
-    }
+    render json: @user
   end
 
 end
