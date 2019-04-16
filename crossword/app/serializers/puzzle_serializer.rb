3895db05d47fa90841570dc105666d3f8dc5a79e
shannon-nabors <shannon.nabors@gmail.com>
2019-04-16 10:52:52 -0400

    added avg to puzzle serializer

--- crossword/app/serializers/puzzle_serializer.rb
+++ crossword/app/serializers/puzzle_serializer.rb
@@ -1,5 +1,5 @@
 class PuzzleSerializer < ActiveModel::Serializer
-  attributes :id, :title, :correct_letters, :across_clues, :down_clues
+  attributes :id, :title, :correct_letters, :across_clues, :down_clues, :average_solve_time
   belongs_to :constructor
   has_many :cells
 end
