862a3a1274aed7c246339b8631b92978f46a7c61
shannon-nabors <shannon.nabors@gmail.com>
2019-04-17 11:19:15 -0400

    added routes

--- crossword/config/routes.rb
+++ crossword/config/routes.rb
@@ -2,11 +2,14 @@ Rails.application.routes.draw do
   resources :puzzles, only: [:destroy]
   resources :solves, only: [:create]
   resources :users, only: [:create]
+  resources :favorites, only: [:create, :destroy]
 
   get "puzzles/user/:id", to: "puzzles#index"
   get "puzzles", to: "puzzles#anonymous_index"
   get "solves/user/:id", to: "solves#by_user"
   get "solves/puzzle/:id", to: "solves#by_puzzle"
+  get "favorites/user/:id", to: "favorites#by_user"
+  get "favorites/puzzle/:id", to: "favorites#by_puzzle"
   post "puzzles/create/:number", to: "puzzles#create"
   patch "puzzles/setup/:id", to: "puzzles#setup"
   patch "puzzles/enter/:id", to: "puzzles#enter"
